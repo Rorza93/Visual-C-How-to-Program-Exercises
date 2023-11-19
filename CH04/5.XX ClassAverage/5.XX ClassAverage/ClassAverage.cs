6542bf075ae4469e390c8c46576f9d9729cb83e3
Rorza93 <r.hambrook93@gmail.com>
2023-11-19 11:21:06 +1100

    Add ClassAverage

---
+++ CH04/5.XX ClassAverage/5.XX ClassAverage/ClassAverage.cs
@@ -0,0 +1,46 @@
+﻿// ClassAverage.cs
+// Solving the class average problem using sentinal-controlled iteration
+
+using System;
+
+class ClassAverage
+{
+    static void Main()
+    {
+        // initialization phase
+        int total = 0; // initialise the sum of grades
+        int gradeCounter = 0; //initialise the # of grades entered so far
+
+        //processing phase
+        //prompt for input and read grade from user
+        Console.Write("Enter grade or -1 to quit: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        // loop until sentinal value is read from the user
+        while (grade != -1)
+        {
+            total = total + grade; // add grade to total
+            gradeCounter = gradeCounter + 1; // increment the counter
+
+            // prompt for input and read grade from the user
+            Console.Write("Enter grade or -1 to quit: ");
+            grade = int.Parse(Console.ReadLine());
+        }
+
+        // termination phase
+        // if the user entered at least one grade
+        if (gradeCounter != 0)
+        {
+            // use number with decimal point to calculate average of the grades
+            double average = (double)total / gradeCounter;
+
+            // displaye the total and average (with two digits of precision)
+            Console.WriteLine($"\nTotal of the {gradeCounter} grades entered is {total}");
+            Console.WriteLine($"Class average is {average:F}");
+        }
+        else // no grades were entered, so output error message
+        {
+            Console.WriteLine("No grades were entered");
+        }
+    }
+}
---
+++ CH04/5.XX Student/5.XX Student/Student.cs
@@ -0,0 +1,68 @@
+﻿// Student.cs
+// Student Class that stores a student name and average.
+using System;
+
+class Student
+{ 
+    public string Name { get; set; }    // property
+    private int average;                // instance variable
+
+    // Constructor initializes Name and Average properties
+    public Student(string studentName, int studentAverage)
+    {
+        Name = studentName;
+        Average = studentAverage;       // sets average instance variable
+    }
+
+    // property to get and set instance variable average
+    public int Average
+    {
+        get // returns the student's average
+        { 
+            return average;
+        }
+        set
+        {
+            // Validate that the int's value is >0 and <= 100; otherwise
+            // keep instance variable average's current value
+            if (value > 0)
+            {
+                if (value <= 100)
+                {
+                    average = value; // assign to instance variable
+                }
+            }
+        }
+    }
+
+    // returns the student's letter grade, based on the average 
+    public string LetterGrade
+    {
+        get
+        { 
+            string letterGrade = string.Empty;  // string.Empty is ""
+
+            if (average >= 90)
+            {
+                letterGrade = "A";
+            }
+            else if (average >= 80)
+            {
+                letterGrade = "B";
+            }
+            else if (average >= 70)
+            {
+                letterGrade = "C";
+            }
+            else if (average >= 60)
+            {
+                letterGrade = "D";
+            }
+            else
+            {
+                letterGrade = "F";
+            }
+            return letterGrade;
+        }
+    }
+}
