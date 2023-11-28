23b433abaa57f14fe1cb2f0d6c91d198ca1520db
Rorza93 <r.hambrook93@gmail.com>
2023-11-29 10:44:38 +1100

    add 5.23 Find the two lagest numbers

---
+++ CH05/5.23 Find the Two Largest Numbers/5.23 Find the Two Largest Numbers/LargestTwo.cs
@@ -0,0 +1,27 @@
+﻿// LargestTwo.cs
+// App used to determine the largest 2 number out of 10 integers input
+
+class Largest
+{
+    static void Main()
+    {
+        int largest = 0;
+        int secondLargest = 0;
+        int number = 0;
+        
+        for (int counter = 0; counter < 10; ++counter)
+        {
+            Console.Write("Enter an Interger value: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number > largest)
+            {
+                secondLargest = largest;
+                largest = number;
+            }
+            
+        }
+
+        Console.WriteLine($"The largest integer entered was {largest} and the second largest was {secondLargest}");
+    }
+}
