cf65778fe125dd3f4789965c7e21850f29e1d7fe
Rorza93 <r.hambrook93@gmail.com>
2023-11-29 11:52:07 +1100

    Add 5.25 Mystery2

---
+++ CH05/5.25 Mystery2/5.25 Mystery2/Mystery2.cs
@@ -0,0 +1,15 @@
+﻿// Ex. 5.25: Mystery2.cs
+using System;
+
+class Mystery2
+{
+    public static void Main()
+    {
+        int number = 50;
+        while (number >= 40)
+            {
+            Console.WriteLine(number * 100 / 50 > 90 ? "A*" : "B");
+            --number;
+            }
+        }
+    }
