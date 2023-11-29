67bd1c52fd81657f0f1ea2a4499e15a0e88b820a
Rorza93 <r.hambrook93@gmail.com>
2023-11-29 12:07:07 +1100

    Add 5.27 Dangling else

---
+++ CH05/5.27 Dangling-else Problem/5.27 Dangling-else Problem/Dangling.cs
@@ -0,0 +1,21 @@
+﻿// Dangling.cs
+using System;
+
+class Dangling
+{
+    static void Main()
+    {
+        int x = 6; int y = 5;
+
+        if (x > 5)
+        {
+            if (y > 5)
+            Console.WriteLine("x and y are > 5");
+        }
+        else
+        {
+            Console.WriteLine("x is <= 5");
+        }
+                
+    }
+}
