11fecb6fd283e87d67f760664155750b6580aaab
Rorza93 <r.hambrook93@gmail.com>
2023-12-02 22:40:51 +1100

    Add 5.32 Decimal equivalence

---
+++ CH05/5.32 Decimal Equivalent of Binary Number/5.32 Decimal Equivalent of Binary Number/Binary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Expanded
+{
+    static void Main()
+    {
+        Console.Write("Enter the binary number you would like to convert: ");
+        string input = Console.ReadLine();
+        
+        int length = input.Length;
+        int total = 0;
+        int positionalValue = 1;
+        while (length > 0)
+        {
+            int current = int.Parse(input[length - 1].ToString());
+            total = total + positionalValue * current;
+            positionalValue *= 2;
+            length--;
+            
+        }
+        Console.WriteLine($"Binary: {input} \nBase 10: {total} ");
+    }
+}
