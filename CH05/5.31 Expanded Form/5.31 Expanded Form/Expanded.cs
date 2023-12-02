7b956b9b2d40ee24e4b6cae2a017d2756d384840
Rorza93 <r.hambrook93@gmail.com>
2023-12-02 20:20:55 +1100

    Add 5.31 expanded form

---
+++ CH05/5.31 Expanded Form/5.31 Expanded Form/Expanded.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Expanded
+{
+    static void Main()
+    {
+        while (true)
+        {
+            int inputLength = 6;
+            
+            Console.Write("Enter a six-digit integer: ");
+            string input = Console.ReadLine();
+
+            if (input.Length != inputLength || !int.TryParse(input, out int number))
+            {
+                Console.WriteLine($"Error: The number must be exactly {inputLength} digits long.");
+                continue;
+            }
+
+            string expandedForm = GetExpandedForm(number);
+            Console.WriteLine($"Expanded form: {expandedForm}");
+            break;
+        }
+    }
+
+    private static string GetExpandedForm(int number)
+    {
+        string result = "";
+        int place = 1;
+        int baseNumber = 10; // base numbering system. we are using base 10
+
+        while (number > 0)
+        {
+            int digit = number % baseNumber;
+            number /= baseNumber;
+
+            string part = $"{digit} x {place}";
+            result = (digit != 0 ? part + (result != "" ? " + " : "") : "") + result;
+            place *= baseNumber;
+        }
+
+        return result;
+    }
+}
